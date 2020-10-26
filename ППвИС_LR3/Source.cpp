b8fc0a8a610b8d5a9f47953fb977af690a56bfef
ALEXEY <usn5555@mail.ru>
2020-10-26 14:05:47 +0300

    Добавьте файлы проекта.

---
+++ ППвИС_LR3/Source.cpp
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+
+using namespace std;
+
+class MyEx:public exception {
+private:
+	int data;
+public:
+	MyEx(string message, int data) :exception(message.c_str()) {
+		this->data = data;
+	}
+	
+	_NODISCARD virtual char const* what() const {
+
+		int size1 = strlen("error value is : ");
+		int size2 = strlen(exception::what());
+		char *arr=new char [size1 + size2];
+		strcpy(arr, "error value is : ");
+		strcat(arr, exception::what());
+		  
+	return arr;
+
+	}
+
+
+};
+
+
+class MyError:public exception {
+
+public:
+	MyError(string message) :exception(message.c_str()) {
+
+	}
+
+};
+
+
+int main() 
+{
+	exception e ("Error in size value");
+
+	try {
+		string s;
+		int size = -8;
+		if (size > 0)
+			s.resize(size);
+		else 
+			throw( MyEx("He",156));
+	}
+	catch (const exception& e) {
+		cout << e.what();
+	}
+	catch (const MyError& e) {
+		cout << e.what();
+	}
+
+	// error value is: He
+
+	return 0;
+}
